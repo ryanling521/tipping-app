538f778f8e22b253f83ee25dc6a8a083350ee24b
Ryan <ryanling521@g.ucla.edu>
2022-08-23 21:57:33 -0700

    almost done with num of ppl logic

--- script.js
+++ script.js
@@ -9,10 +9,26 @@ function displayNumPeople() {
     console.log(numPeople.value);
 }
 
+let costArray = [];
+
+// function that creates increments divs "cost of each meal"
+function addMeal() {
+    for (let i = 0; i < numPeople.value; i++) {
+        costArray.push('meal-${i}')
+    }
+    costArray.forEach(function(e) {
+        let div = document.createElement('div');
+        div.classList.add("form-group"); 
+        div.classList.add("each-meal");
+        div.textContent += "'hello!";
+        costMealGroup.append(div);
+    })
+}
+
 // displays the number of people
 button.addEventListener("click", displayNumPeople);
 
 // constantly checks the number of people
-// setInterval(displayNumPeople, 100);
+setInterval(addMeal(), 100);
 
 
